d35b8fece0595c649f341177395b92764fba0b65
Maninder Taggar <manindersingh030@gmail.com>
2017-02-03 22:29:21 +0530

    -

--- app/src/main/java/com/goldducks/a8crack/MainActivity.java
+++ app/src/main/java/com/goldducks/a8crack/MainActivity.java
@@ -7,36 +7,35 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.RelativeLayout;
 
-public class MainActivity extends Activity{
-    View guidlineView;
-    View rotateClockwiseView, rotateAntiClockwiseView;
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        rotateClockwiseView = findViewById(R.id.rotateClockWise);
-        rotateAntiClockwiseView = findViewById(R.id.rotateAntiClockWise);
-
-        rotateClockwiseView.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View view, MotionEvent motionEvent) {
-                return false;
-            }
-        });
-        rotateAntiClockwiseView.setOnTouchListener(this);
-
-        DisplayMetrics displaymetrics = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
-        int height = displaymetrics.heightPixels;
-
-        guidlineView = findViewById(R.id.guidlineView);
-        RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) guidlineView.getLayoutParams();
-        params.width = height * 2;
-        params.height = 10;
-        guidlineView.setLayoutParams(params);
-
     }
-
 }
+
+//        View guidlineView;
+//        View rotateClockwiseView, rotateAntiClockwiseView;
+
+//        rotateClockwiseView = findViewById(R.id.rotateClockWise);
+//        rotateAntiClockwiseView = findViewById(R.id.rotateAntiClockWise);
+//
+//        rotateClockwiseView.setOnTouchListener(new View.OnTouchListener() {
+//            @Override
+//            public boolean onTouch(View view, MotionEvent motionEvent) {
+//                return false;
+//            }
+//        });
+//
+//        DisplayMetrics displaymetrics = new DisplayMetrics();
+//        getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
+//        int height = displaymetrics.heightPixels;
+//
+//        guidlineView = findViewById(R.id.guidlineView);
+//        RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) guidlineView.getLayoutParams();
+//        params.width = height * 2;
+//        params.height = 10;
+//        guidlineView.setLayoutParams(params);
+//
