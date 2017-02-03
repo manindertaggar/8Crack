03ed35ae4c59f9231515a006f366ee1ad71f9b93
Maninder Taggar <manindersingh030@gmail.com>
2017-02-03 23:59:43 +0530

    -

--- app/src/main/java/com/goldducks/a8crack/MainActivity.java
+++ app/src/main/java/com/goldducks/a8crack/MainActivity.java
@@ -2,21 +2,49 @@ package com.goldducks.a8crack;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.RelativeLayout;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements RotationGestureDetector.OnRotationGestureListener {
+    private RotationGestureDetector mRotationDetector;
+    private View activity_main;
+    View guidlineView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        guidlineView = findViewById(R.id.guidlineView);
+
+        mRotationDetector = new RotationGestureDetector(this);
+        activity_main = findViewById(R.id.activity_main);
+        activity_main.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                mRotationDetector.onTouchEvent(motionEvent);
+                return false;
+            }
+        });
+
+
     }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        mRotationDetector.onTouchEvent(event);
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    public void OnRotation(RotationGestureDetector rotationDetector) {
+        float angle = -rotationDetector.getAngle();
+        guidlineView.setRotation(angle);
+        Log.d("RotationGestureDetector", "Rotation: " + Float.toString(angle));
+    }
+
 }
 
-//        View guidlineView;
 //        View rotateClockwiseView, rotateAntiClockwiseView;
 
 //        rotateClockwiseView = findViewById(R.id.rotateClockWise);
