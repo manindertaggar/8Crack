b29d00d7c7f7162d06f4e7b313a2e4a5eb1a8d13
Maninder Taggar <manindersingh030@gmail.com>
2017-02-03 22:26:06 +0530

    -

---
+++ app/src/main/java/com/goldducks/a8crack/MainActivity.java
@@ -0,0 +1,42 @@
+package com.goldducks.a8crack;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+public class MainActivity extends Activity{
+    View guidlineView;
+    View rotateClockwiseView, rotateAntiClockwiseView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        rotateClockwiseView = findViewById(R.id.rotateClockWise);
+        rotateAntiClockwiseView = findViewById(R.id.rotateAntiClockWise);
+
+        rotateClockwiseView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                return false;
+            }
+        });
+        rotateAntiClockwiseView.setOnTouchListener(this);
+
+        DisplayMetrics displaymetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
+        int height = displaymetrics.heightPixels;
+
+        guidlineView = findViewById(R.id.guidlineView);
+        RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) guidlineView.getLayoutParams();
+        params.width = height * 2;
+        params.height = 10;
+        guidlineView.setLayoutParams(params);
+
+    }
+
+}
