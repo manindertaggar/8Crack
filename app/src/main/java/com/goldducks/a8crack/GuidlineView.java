3781af6b8c1e96c8de01dfa7b9cee5ac8c7493ec
Maninder Taggar <manindersingh030@gmail.com>
2017-02-04 10:52:05 +0530

    moved code to guidlineView

---
+++ app/src/main/java/com/goldducks/a8crack/GuidlineView.java
@@ -0,0 +1,54 @@
+package com.goldducks.a8crack;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * Created by root on 4/2/17.
+ */
+
+public class GuidlineView implements RotationGestureDetector.OnRotationGestureListener {
+    private Context context;
+    private View contentView;
+    private View guidlineView;
+    private RotationGestureDetector rotationGestureDetector;
+
+    public GuidlineView(Context context) {
+        this.context = context;
+
+        rotationGestureDetector = new RotationGestureDetector(this);
+
+        intializeViews();
+        setListeners();
+    }
+
+    private void intializeViews() {
+        contentView = LayoutInflater.from(context).inflate(R.layout.layout_guidline, null);
+        guidlineView = contentView.findViewById(R.id.guidlineView);
+
+    }
+
+    private void setListeners() {
+        contentView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                rotationGestureDetector.onTouchEvent(motionEvent);
+                return false;
+            }
+        });
+
+    }
+
+    @Override
+    public void OnRotation(RotationGestureDetector rotationDetector) {
+        float angle = -rotationDetector.getAngle();
+        guidlineView.setRotation(angle);
+    }
+
+
+    public View getView() {
+        return contentView;
+    }
+}
--- app/src/main/java/com/goldducks/a8crack/MainActivity.java
+++ app/src/main/java/com/goldducks/a8crack/MainActivity.java
@@ -2,68 +2,18 @@ package com.goldducks.a8crack;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.MotionEvent;
-import android.view.View;
+import android.widget.LinearLayout;
 
-public class MainActivity extends Activity implements RotationGestureDetector.OnRotationGestureListener {
-    private RotationGestureDetector mRotationDetector;
-    private View activity_main;
-    View guidlineView;
+public class MainActivity extends Activity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        guidlineView = findViewById(R.id.guidlineView);
-
-        mRotationDetector = new RotationGestureDetector(this);
-        activity_main = findViewById(R.id.activity_main);
-        activity_main.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View view, MotionEvent motionEvent) {
-                mRotationDetector.onTouchEvent(motionEvent);
-                return false;
-            }
-        });
-
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.ll);
+        linearLayout.addView(new GuidlineView(this).getView());
 
     }
 
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        mRotationDetector.onTouchEvent(event);
-        return super.onTouchEvent(event);
-    }
-
-    @Override
-    public void OnRotation(RotationGestureDetector rotationDetector) {
-        float angle = -rotationDetector.getAngle();
-        guidlineView.setRotation(angle);
-        Log.d("RotationGestureDetector", "Rotation: " + Float.toString(angle));
-    }
 
 }
-
-//        View rotateClockwiseView, rotateAntiClockwiseView;
-
-//        rotateClockwiseView = findViewById(R.id.rotateClockWise);
-//        rotateAntiClockwiseView = findViewById(R.id.rotateAntiClockWise);
-//
-//        rotateClockwiseView.setOnTouchListener(new View.OnTouchListener() {
-//            @Override
-//            public boolean onTouch(View view, MotionEvent motionEvent) {
-//                return false;
-//            }
-//        });
-//
-//        DisplayMetrics displaymetrics = new DisplayMetrics();
-//        getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
-//        int height = displaymetrics.heightPixels;
-//
-//        guidlineView = findViewById(R.id.guidlineView);
-//        RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) guidlineView.getLayoutParams();
-//        params.width = height * 2;
-//        params.height = 10;
-//        guidlineView.setLayoutParams(params);
-//
