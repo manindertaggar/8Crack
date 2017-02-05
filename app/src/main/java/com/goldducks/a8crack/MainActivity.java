c5eafb8e5bd1fdc45a3fb6e0b5f2bc7aa1bf8266
Maninder Taggar <manindersingh030@gmail.com>
2017-02-05 09:42:00 +0530

    app on top done

--- app/src/main/java/com/goldducks/a8crack/MainActivity.java
+++ app/src/main/java/com/goldducks/a8crack/MainActivity.java
@@ -3,6 +3,7 @@ package com.goldducks.a8crack;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 
 public class MainActivity extends Activity {
 
@@ -14,7 +15,10 @@ public class MainActivity extends Activity {
         new ViewManager(getApplicationContext());
 
         //  PermissionManager.checkForOverlayPermission(this);
-        //  startService(new Intent(this, CrackService.class));
+        //  startService(new Intent(this, CrackServidce.class));
+
+        Intent intent = new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS);
+        startActivity(intent);
 
         startService(new Intent(this, RunningAppDetectorService.class));
 
--- app/src/main/java/com/goldducks/a8crack/RunningAppDetectorService.java
+++ app/src/main/java/com/goldducks/a8crack/RunningAppDetectorService.java
@@ -2,18 +2,18 @@ package com.goldducks.a8crack;
 
 import android.app.ActivityManager;
 import android.app.Service;
+import android.app.usage.UsageStats;
+import android.app.usage.UsageStatsManager;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.IBinder;
-import android.util.Log;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Iterator;
 import java.util.List;
+import java.util.SortedMap;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.TreeMap;
 
 /**
  * Created by root on 4/2/17.
@@ -21,7 +21,15 @@ import java.util.TimerTask;
 
 public class RunningAppDetectorService extends Service {
     private static final String TAG = RunningAppDetectorService.class.getCanonicalName();
-    List stalkList = new ArrayList();
+    ActivityManager am;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        am = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+    }
+
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -35,119 +43,36 @@ public class RunningAppDetectorService extends Service {
         timer.scheduleAtFixedRate(new TimerTask() {
 
             public void run() {
-                final ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
-                final List<ActivityManager.RunningTaskInfo> services = activityManager.getRunningTasks(Integer.MAX_VALUE);
-                for (int i = 0; i < services.size(); i++) {
-                    if (!stalkList.contains(services.get(i).baseActivity.getPackageName())) {
-                        stalkList.add(services.get(i).baseActivity.getPackageName());
-                    }
-                }
-
-                List<ActivityManager.RunningAppProcessInfo> procInfos = activityManager.getRunningAppProcesses();
-                for (int i = 0; i < procInfos.size(); i++) {
-
-                    ArrayList<String> runningPkgs = new ArrayList<String>(Arrays.asList(procInfos.get(i).pkgList));
-
-                    Collection diff = subtractSets(runningPkgs, stalkList);
-
-                    if (diff != null) {
-                        stalkList.removeAll(diff);
-                    }
-                }
-                Log.d(TAG, "run: " +getForegroundApp());
+               String appOnTop= getAppRunningOnTop();
 
             }
-        }, 20000, 6000);  // every 6 seconds
+
+        }, 20000, 5000);
 
 
         return START_STICKY;
     }
 
-
-    private ActivityManager.RunningAppProcessInfo getForegroundApp() {
-        ActivityManager.RunningAppProcessInfo result = null, info = null;
-
-        final ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
-
-        List<ActivityManager.RunningAppProcessInfo> l = activityManager.getRunningAppProcesses();
-        Iterator<ActivityManager.RunningAppProcessInfo> i = l.iterator();
-        while (i.hasNext()) {
-            info = i.next();
-            if (info.importance == ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND
-                    && !isRunningService(info.processName)) {
-                result = info;
-                break;
+    private String getAppRunningOnTop() {
+        if (Build.VERSION.SDK_INT >= 21) {
+            String currentApp = null;
+            UsageStatsManager usm = (UsageStatsManager) this.getSystemService(Context.USAGE_STATS_SERVICE);
+            long time = System.currentTimeMillis();
+            List<UsageStats> applist = usm.queryUsageStats(UsageStatsManager.INTERVAL_DAILY, time - 1000 * 1000, time);
+            if (applist != null && applist.size() > 0) {
+                SortedMap<Long, UsageStats> mySortedMap = new TreeMap<>();
+                for (UsageStats usageStats : applist) {
+                    mySortedMap.put(usageStats.getLastTimeUsed(), usageStats);
+                }
+                if (mySortedMap != null && !mySortedMap.isEmpty()) {
+                    currentApp = mySortedMap.get(mySortedMap.lastKey()).getPackageName();
+                }
             }
+            return currentApp;
+        } else {
+            ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+            String mm = (manager.getRunningTasks(1).get(0)).topActivity.getPackageName();
+            return mm;
         }
-        return result;
     }
-
-    private boolean isRunningService(String processName) {
-        if (processName == null)
-            return false;
-
-        ActivityManager.RunningServiceInfo service;
-
-        final ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
-
-        List<ActivityManager.RunningServiceInfo> l = activityManager.getRunningServices(9999);
-        Iterator<ActivityManager.RunningServiceInfo> i = l.iterator();
-        while (i.hasNext()) {
-            service = i.next();
-            if (service.process.equals(processName))
-                return true;
-        }
-        return false;
-    }
-
-    private boolean isRunningApp(String processName) {
-        if (processName == null)
-            return false;
-
-        ActivityManager.RunningAppProcessInfo app;
-
-        final ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
-
-        List<ActivityManager.RunningAppProcessInfo> l = activityManager.getRunningAppProcesses();
-        Iterator<ActivityManager.RunningAppProcessInfo> i = l.iterator();
-        while (i.hasNext()) {
-            app = i.next();
-            if (app.processName.equals(processName) && app.importance != ActivityManager.RunningAppProcessInfo.IMPORTANCE_SERVICE)
-                return true;
-        }
-        return false;
-    }
-
-
-    private boolean checkifThisIsActive(ActivityManager.RunningAppProcessInfo target) {
-        boolean result = false;
-        ActivityManager.RunningTaskInfo info;
-
-        if (target == null)
-            return false;
-
-        final ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
-
-        List<ActivityManager.RunningTaskInfo> l = activityManager.getRunningTasks(9999);
-        Iterator<ActivityManager.RunningTaskInfo> i = l.iterator();
-
-        while (i.hasNext()) {
-            info = i.next();
-            if (info.baseActivity.getPackageName().equals(target.processName)) {
-                result = true;
-                break;
-            }
-        }
-
-        return result;
-    }
-
-
-    // what is in b that is not in a ?
-    public static Collection subtractSets(Collection a, Collection b) {
-        Collection result = new ArrayList(b);
-        result.removeAll(a);
-        return result;
-    }
-
 }
