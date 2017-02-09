8a36628b094a6a63499c1e38c0da38fcacf4942d
Maninder Singh <manindersingh030@gmail.com>
2017-02-09 22:05:42 +0530

    PackageManager done

--- app/src/main/java/com/goldducks/a8crack/PoolPackageManager.java
+++ app/src/main/java/com/goldducks/a8crack/PoolPackageManager.java
@@ -1,7 +1,9 @@
 package com.goldducks.a8crack;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 
 /**
  * Created by Maninder on 09-02-2017.
@@ -9,9 +11,8 @@ import android.content.pm.PackageManager;
 
 public class PoolPackageManager {
 
-    public boolean isPackageInstalled(Context context) {
+    public static boolean isInstalled(Context context) {
         try {
-
             PackageManager packageManager = context.getPackageManager();
             packageManager.getPackageInfo(Constants.POOL_PACKAGE_NAME, PackageManager.GET_ACTIVITIES);
             return true;
@@ -19,4 +20,23 @@ public class PoolPackageManager {
             return false;
         }
     }
+
+    public static boolean openApplication(Context context) {
+        Intent launchIntent = context.getPackageManager().getLaunchIntentForPackage(Constants.POOL_PACKAGE_NAME);
+        if (launchIntent != null) {
+            context.startActivity(launchIntent);
+            return true;
+        }
+        return false;
+    }
+
+    public static void openInPlayerStore(Context context) {
+        try {
+            context.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + Constants.POOL_PACKAGE_NAME)));
+        } catch (android.content.ActivityNotFoundException anfe) {
+            context.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://play.google.com/store/apps/details?id=" + Constants.POOL_PACKAGE_NAME)));
+        }
+    }
+
 }
+
